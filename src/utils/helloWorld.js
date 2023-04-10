624a79b160ce6be898c6dcd86a7c470d1e51b80b
mirjalol03 <jabborovmirjalol03@gmail.com>
2023-04-11 00:07:13 +0500

    Created HellowwithFlags array added style and created Card in root.jsx

---
+++ src/utils/helloWorld.js
@@ -0,0 +1,62 @@
+export const helloWithFlags = [
+  {
+    Message: "Hello World!",
+    Flag: "https://flagcdn.com/w320/us.png",
+  },
+  {
+    Message: "こんにちは世界!",
+    Flag: "https://flagcdn.com/w320/jp.png",
+  },
+  {
+    Message: "Привет мир!",
+    Flag: "https://flagcdn.com/w320/ru.png",
+  },
+  {
+    Message: "नमस्ते दुनिया!",
+    Flag: "https://flagcdn.com/w320/in.png",
+  },
+  {
+    Message: "Ciao Mondo!",
+    Flag: "https://flagcdn.com/w320/it.png",
+  },
+  {
+    Message: "Hola Mundo!",
+    Flag: "https://flagcdn.com/w320/es.png",
+  },
+  {
+    Message: "你好，世界!",
+    Flag: "https://flagcdn.com/w320/cn.png",
+  },
+  {
+    Message: "مرحبا بالعالم!",
+    Flag: "https://flagcdn.com/w320/sa.png",
+  },
+  {
+    Message: "Bonjour le monde!",
+    Flag: "https://flagcdn.com/w320/fr.png",
+  },
+  {
+    Message: "Hallo Welt!",
+    Flag: "https://flagcdn.com/w320/de.png",
+  },
+  {
+    Message: "Olá Mundo!",
+    Flag: "https://flagcdn.com/w320/br.png",
+  },
+  {
+    Message: "안녕하세요 세상!",
+    Flag: "https://flagcdn.com/w320/kr.png",
+  },
+  {
+    Message: "ওহে বিশ্ব!",
+    Flag: "https://flagcdn.com/w320/bd.png",
+  },
+  {
+    Message: "Habari Dunia!",
+    Flag: "https://flagcdn.com/w320/ke.png",
+  },
+  {
+    Message: "Hallo Wereld!",
+    Flag: "https://flagcdn.com/w320/nl.png",
+  },
+];
